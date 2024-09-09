13e56d367b931faec7beeaaf6f8a92076e81660b
jawade.arpeet <jawade.arpeet@gmail.com>
2024-09-09 18:25:09 +0530

    add banner.tsx to homepage

--- src/app/(routes)/(marketing)/page.tsx
+++ src/app/(routes)/(marketing)/page.tsx
@@ -1,9 +1,11 @@
-import { Header } from "@/app/components/marketing/header";
+import Header from "@/app/components/marketing/header";
+import Banner from "@/app/components/marketing/banner";
 
 function HomePage() {
   return (
     <div>
       <Header />
+      <Banner />
     </div>
   );
 }
---
+++ src/app/components/marketing/banner.tsx
@@ -0,0 +1,27 @@
+import { Button } from "@/app/components/ui/button";
+
+function Banner() {
+  return (
+    <div className={"flex flex-col items-center gap-y-1 mt-20"}>
+      <small className={"text-lg font-bold"}>counsel</small>
+      <h1
+        className={
+          "text-4xl text-center lg:mt-4 mx-auto max-w-[80%] lg:max-w-3xl lg:text-5xl"
+        }
+      >
+        Get answers. Find inspiration. Be more productive.
+      </h1>
+      <p
+        className={
+          "text-center mx-auto max-w-[90%] mt-1 lg:mt-4 text-lg lg:max-w-xl"
+        }
+      >
+        Free to use. Easy to try. Just ask and ChatGPT can help with writing,
+        learning, brainstorming, and more.
+      </p>
+      <Button className={"mt-2"}>Start Now</Button>
+    </div>
+  );
+}
+
+export default Banner;
