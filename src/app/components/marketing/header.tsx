f4f8e2d88fe437130a83889234602fe470aa8c01
jawade.arpeet <jawade.arpeet@gmail.com>
2024-09-13 13:44:53 +0530

    update import method.

--- src/app/components/marketing/header.tsx
+++ src/app/components/marketing/header.tsx
@@ -1,12 +1,15 @@
-import { Button } from "@/app/components/ui/button";
+import Button from "@/app/components/ui/button";
 import Logo from "@/app/components/ui/logo";
+import Link from "next/link";
 
 function Header() {
   return (
     <header className={"max-w-screen-lg mx-auto px-6 py-4"}>
       <nav className={"flex items-center justify-between"}>
         <Logo />
-        <Button>Get Started</Button>
+        <Link href={"/sign-up"} passHref={true}>
+          <Button>Get Started</Button>
+        </Link>
       </nav>
     </header>
   );
